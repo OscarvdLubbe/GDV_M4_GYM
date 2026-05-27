89ca1900c2e8b0d177edd7c613ee0fc59f69684c
OscarvdLubbe <40429@ma-web.nl>
2026-05-27 11:24:21 +0200

    animations en movement

--- Assets/scripts/InputPlayer.cs
+++ Assets/scripts/InputPlayer.cs
@@ -7,17 +7,25 @@ public class InputPlayer : MonoBehaviour
 {
     [SerializeField] private InputActionAsset input;
     [SerializeField] private string actionMapName = "Player1";
+    private Animator animator;
     private InputActionMap map;
     private InputAction moveAction;
     private InputAction jumpAction;
     private InputAction sprintAction;
     private Rigidbody rb;
+    private float movespeed = 5f;
+    private float jumpForce = 200f;
+    private bool Grounded;
+
+
     void Awake()
     {
         map = input.FindActionMap(actionMapName);
         moveAction = map.FindAction("Move");
         jumpAction = map.FindAction("Jump");
         sprintAction = map.FindAction("Sprint");
+
+        animator = GetComponent<Animator>();
     }
     void OnEnable()
     {
@@ -31,14 +39,17 @@ public class InputPlayer : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpAction.WasPressedThisFrame())
+        if (jumpAction.WasPressedThisFrame() && Grounded == true)
         {
             Debug.Log("Jump has been pressed");
+            rb.AddForce(Vector3.up * jumpForce , ForceMode.Force);
+            animator.SetTrigger("JumpTrigger");
         }
         else if (jumpAction.IsPressed())
         {
@@ -48,8 +59,33 @@ public class InputPlayer : MonoBehaviour
         {
             Debug.Log("no more jump");
         }
+        animator.SetFloat("Speed", movespeed);
+
+        
+        
+
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        transform.Translate(moveInput.y * transform.forward * Time.deltaTime * 100f, Space.World);
+
+        float currentMoveSpeed = moveInput.y * movespeed * Time.deltaTime;
+
+        transform.Translate(transform.forward * currentMoveSpeed, Space.World);
         transform.Rotate(Vector3.up , moveInput.x * Time.deltaTime * 100f, Space.World);
+
+        animator.SetFloat("Speed", currentMoveSpeed);
+        Debug.Log(Grounded);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Grounded = true;   
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Grounded = false;   
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 layout
 ![bomboclat](/Schermafbeelding%202026-05-13%20112424.png)
+
+animatie en movement
+![](/gif/knight%20animationGYM%20(1).gif)
